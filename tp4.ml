7df3c3058166882f0a44408c9611d06e8174eb42
Alan <alanbignon2@gmail.com>
2022-02-25 18:26:40 +0100

    created func for bataille

--- tp4.ml
+++ tp4.ml
@@ -89,33 +89,30 @@ let getValueOfFace f =
     | Valet -> "Valet"
     | Dame -> "Dame"
     | Roi -> "Roi"
-    | As -> "As"
-;;
-
+    | As -> "As";;
 let getValueOfCouleur c = 
     match c with 
     | Carreau -> "Carreau"
     | Trefle -> "Trefle"
     | Coeur -> "Coeur"
-    | Pique -> "Pique"
+    | Pique -> "Pique";;
 
 let print_carte carte =
     match carte with
-    | Carte(f, c) -> print_string (getValueOfFace f) ; print_string " de " ;print_string (getValueOfCouleur c); print_newline();
-;;
-let create_deck ()= 
+    | Carte(f, c) -> print_string (getValueOfFace f) ; print_string " de " ;print_string (getValueOfCouleur c); print_newline();;
+
+let create_deck () = 
     let rec sub_create_deck nb i =
         match i with
         | 1 -> Carte (getFace nb,Carreau):: if nb == 2 then [] else sub_create_deck (nb-1) i
-        | 2 -> Carte (getFace nb,Coeur):: if nb == 2 then sub_create_deck 14 (i-1) else sub_create_deck (nb-1) i
+        | 2 -> Carte (getFace nb, Coeur):: if nb == 2 then sub_create_deck 14 (i-1) else sub_create_deck (nb-1) i
         | 3 -> Carte (getFace nb,Trefle):: if nb == 2 then sub_create_deck 14 (i-1) else sub_create_deck (nb-1) i
         | 4 -> Carte (getFace nb, Pique):: if nb == 2 then sub_create_deck 14 (i-1) else sub_create_deck (nb-1) i
         | _ -> failwith "errror create deck" 
     in sub_create_deck 14 4
 ;;
 
-
-let deck = create_deck ();;
+(* let deck = create_deck ();; *)
 
 let rec print_list list =  
     match list with
@@ -152,13 +149,81 @@ print_list l1;;
 
 (* Question 4 *)
 let rec distribue d l1 l2 = 
-    if List.length l1 = List.length d / 2
+    if List.length l1 = 26
     then
         (l1,l2)
     else 
         match d with
-            | [] -> failwith "error"
-            | x::y::t -> distribue d (x::l1) (y::l2)
+            | x::y::t -> distribue t (x::l1) (y::l2)
+            | _ -> failwith "error"
+;;
+(* Exemple 
+let deck = create_deck ();; 
+let deck = melange deck 0 1000;;
+
+let l1,l2 = distribue deck [] [];;
+print_newline();;
+print_list l1;;
+
+print_newline();;
+print_list l2;;
+*)
+
+
+
+(* Question 6 *)
+let un_tour deck1 deck2 = 
+    match (deck1, deck2) with
+    | (x1::d1, x2::d2) -> let res = compare x1 x2 in 
+        if res = 1 
+        then 
+            (deck1@[x2], d2) 
+        else 
+        begin 
+            if res = 2 
+            then
+                (d1, deck2@[x1]) 
+            else 
+                (d1@[x1], d2@[x2])
+        end
+    | ([], d2) -> ([], d2)
+    | (d1, []) -> (d1, [])
 ;;
 
-(* Question 5*)
+
+let liste_vide list =
+    match list with
+    | [] -> true
+    | _ -> false
+;;
+
+
+ let rec bataille d1 d2 = 
+    if liste_vide d1 
+    then
+        print_string "J2 à gagné"
+    else
+    if liste_vide d2
+    then
+        print_string "J1 à gagné"
+    else 
+        let (deck1, deck2) = un_tour d1 d2 in
+        print_string "début deck 1 :" ;
+        print_newline () ;
+        print_list deck1;
+        print_string "fin deck 1, début deck 2 : ";
+        print_newline ();
+        print_list deck2 ;
+        print_string "fin deck 2." ;
+        print_newline ();
+        bataille deck1 deck2
+;;
+
+
+let bataille_ouverte () = 
+    let deck = create_deck () in
+    let deck = melange deck 0 200 in
+    let deck1, deck2 = distribue deck [] [] in
+    bataille deck1 deck2;;
+
+bataille_ouverte();; 
